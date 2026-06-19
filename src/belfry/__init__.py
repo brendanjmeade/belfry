29b0420c95511b2b5110bfeea7ea0a26ce96053b
Brendan Meade <brendanjmeade@gmail.com>
2026-06-18 23:54:51 -0400

    Release 0.1.1

--- src/belfry/__init__.py
+++ src/belfry/__init__.py
@@ -1,3 +1,3 @@
-"""belfry -- Ring a Bell: a Textual TUI for rediscovering what your Python scripts do."""
+"""belfry -- A Textual TUI for rediscovering what your Python scripts do."""
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
